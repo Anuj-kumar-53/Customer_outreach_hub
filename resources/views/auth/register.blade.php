2aae4304301e6bd477ab6240a13851dcc7708629
Anuj-kumar-53 <anujksah123@gmail.com>
2026-05-16 23:09:17 +0530

    made landing page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -55,14 +55,22 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+        <div class="flex items-center justify-between mt-4">
+            <a class=" text-sm  rounded-md border p-2 bg-black text-white text-center" href="{{ url('/') }}">
+                
+               &larr;{{ __('Back') }}
+               
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <div class="flex items-center">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+                <x-primary-button class="ms-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+
+            </div>
         </div>
     </form>
 </x-guest-layout>
